eae0c47102ec58ceae4f7d9fc66e2ae6d8b0a63d
sowousmane4811@gmail.com <ousmane>
2023-05-26 20:47:53 +0200

    add another endpoint

--- weather.py
+++ weather.py
@@ -7,13 +7,13 @@ load_dotenv()  # take environment variables from .env.
 #initialize the Flask application
 app = Flask(__name__)
 
-@app.route('/forecast', methods=['GET'])
-def get_weather():
-    city = request.args.get('city')
-    if city is None:
-        abort(400, 'Missing argument city')
+def get_weather(city_name=None):   
     data = {}
-    data['q'] = request.args.get('city')
+    if city_name:
+        data['q'] = request.args.get('city')
+    else:
+        data['q'] = request.form['city'] 
+
     data['appid'] = os.getenv("OPEN_WEATHER_TOKEN")
     data['units'] = 'metric'
 
@@ -21,7 +21,30 @@ def get_weather():
     url = 'http://api.openweathermap.org/data/2.5/forecast'
     full_url = url + '?' + url_values
     data = urllib.request.urlopen(full_url)
+    print(data)
+    data=json.loads(data.read().decode('utf8'))
+    print(data)
+    return data
 
-    resp = Response(data)
-    resp.status_code = 200
-    return render_template('index.html', title='Weather App', data=json.loads(data.read().decode('utf8')))
+@app.route("/search-city", methods=["POST"])
+def validate_user():
+    if request.method == "POST":
+        print(request.form['city'])
+    data=get_weather()
+    return render_template('index.html', title='Weather App', data=data)
+
+@app.route('/',methods=['GET', 'POST'])
+def home():
+    result = None
+
+    if request.method == 'POST':
+        # Handle form submission
+        result = validate_user()  # Replace with your actual function
+
+    return render_template('home.html', title="home",result=result)
+
+
+@app.route('/forecast', methods=['GET'])
+def get_wea():    
+    data=get_weather(request.args.get('city'))
+    return render_template('index.html', title='App', data=data)
